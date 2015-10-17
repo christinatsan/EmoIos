eb09fa429e7942ed8df47fe2f537abe29b09baa8
christinatsan <ctsango000@citymail.cuny.edu>
2015-10-17 19:50:43 -0400

    Initial Commit

---
+++ together/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  together
+//
+//  Created by Christina Tsangouri on 10/17/15.
+//  Copyright © 2015 Christina Tsangouri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ together/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  together
+//
+//  Created by Christina Tsangouri on 10/17/15.
+//  Copyright © 2015 Christina Tsangouri. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
